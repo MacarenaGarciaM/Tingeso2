c554bdf9a749ed7386241c5136646af195ccd8a5
Macarena García Morales <macarena.garcia@usach.cl>
2026-01-06 23:22:48 -0300

    keycloak ruta correcta?

--- gateway-server/src/main/java/com/example/gatewayserver/config/SecurityConfig.java
+++ gateway-server/src/main/java/com/example/gatewayserver/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -13,23 +12,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-
-        http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(ex -> ex
-                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-
-                        // si /auth/** lo usas para cosas públicas (ej: health o endpoints sin token)
-                        .pathMatchers("/auth/**").permitAll()
-
-                        // lo demás con token
-                        .anyExchange().authenticated()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
-
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .cors(Customizer.withDefaults())   // 👈 ESTA LÍNEA
+                .cors(Customizer.withDefaults())
                 .authorizeExchange(ex -> ex
                         .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .pathMatchers("/auth/**").permitAll()
@@ -37,6 +22,5 @@ public class SecurityConfig {
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}))
                 .build();
-
     }
 }
